2f57ee401ef86ab6cc75b504f884a0b9cab5c1d9
Maurice Claessen <mc1984@gmail.com>
2023-09-23 13:29:32 +0200

    Added functionality for color and sizing
    Added the following:
    
    - Color buttons
    - A Change Size button that will prompt the user for input and accept values between 10 and 100

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const CANVASSIZE = 800;
+const COLORS = {
+    red: getStylesheetVariable('red'),
+    green: getStylesheetVariable('green'),
+    blue: getStylesheetVariable('blue'),
+    black: 'black',
+    white: 'white',
+    default: 'grey'
+}
+
+const DEFAULT_COLOR = COLORS['default'];
+
+const CANVAS_WIDTH_HEIGHT = 800;
 
 const canvas = document.querySelector('.canvas');
-const DEFAULTSIZE = 16;
+const DEFAULT_CANVAS_SIZE = 16;
 
-let currentCanvasSize = DEFAULTSIZE;
+let currentCanvasSize = DEFAULT_CANVAS_SIZE;
+let selectedColor = DEFAULT_COLOR;
+
+const sizeButton = document.querySelector('#canvas-size');
+sizeButton.addEventListener('click', setCanvasSize);
+
+const colorButtons = document.querySelectorAll('.color-button');
+colorButtons.forEach((button) => {
+    button.addEventListener('click', e => {
+        setSelectedColor(e.target.textContent.toLowerCase());
+    });
+
+    button.lastElementChild.style.backgroundColor = COLORS[button.textContent.toLowerCase()];
+});
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', resetCanvas);
@@ -11,6 +35,7 @@ resetButton.addEventListener('click', resetCanvas);
 resetCanvas();
 
 function resetCanvas() {
+    selectedColor = DEFAULT_COLOR;
     canvas.innerHTML = "";
 
     drawCanvas(currentCanvasSize);
@@ -29,18 +54,40 @@ function drawCanvas(size) {
     for (let i = 0; i < numberOfSquares; i++) {
         const square = document.createElement('div');
         square.classList = 'grid-square';
-        square.style = `width: ${Math.floor(CANVASSIZE / size)}px; height: ${Math.floor(CANVASSIZE / size)}px;`;
+        square.style.width = `${Math.floor(CANVAS_WIDTH_HEIGHT / size)}px`;
+        square.style.height = `${Math.floor(CANVAS_WIDTH_HEIGHT / size)}px`;
+        square.style.backgroundColor = 'white';
 
         canvas.appendChild(square);
     }
 }
 
 function colorSquare(square) {
-    // square.classList = '';
-    // square.classList = `grid-square ${getSquareColor()}`;
     square.style.backgroundColor = `${getSquareColor()}`;
 }
 
 function getSquareColor() {
-    return 'black';
+    return selectedColor;
+}
+
+function getStylesheetVariable(name) {
+    return getComputedStyle(document.documentElement).getPropertyValue(`--${name}`);
+}
+
+function setSelectedColor(color) {
+    selectedColor = COLORS[color]
+    console.log(selectedColor);
+}
+
+function setCanvasSize() {
+    let validSize = false;
+    let answer = "";
+
+    while (!validSize) {
+        answer = prompt(`Current canvas size is ${currentCanvasSize}x${currentCanvasSize}.` +
+            `\nEnter a size or leave blank to leave at ${currentCanvasSize}x${currentCanvasSize}`);
+        validSize = (answer >= 10 && answer <= 100);
+    }
+    currentCanvasSize = answer;
+    resetCanvas();
 }
