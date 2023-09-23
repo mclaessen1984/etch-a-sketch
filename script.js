38c0eeb8a0a6b997ad3c8b81eace71401439a586
Maurice Claessen <mc1984@gmail.com>
2023-09-23 11:51:27 +0200

    Completed basic functionality
    Completed functionality:
    
    - Draw a 16x16 grid
    - Color squares black on mouseover
    - Reset squares when clicking the Reset canvas button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const canvas = document.querySelector('.canvas');
-const defaultCanvasSize = 16;
+const CANVASSIZE = 800;
 
-let currentCanvasSize = defaultCanvasSize;
+const canvas = document.querySelector('.canvas');
+const DEFAULTSIZE = 16;
+
+let currentCanvasSize = DEFAULTSIZE;
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', resetCanvas);
@@ -12,12 +14,33 @@ function resetCanvas() {
     canvas.innerHTML = "";
 
     drawCanvas(currentCanvasSize);
+
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.addEventListener('mouseover', e => {
+            colorSquare(e.target)
+        });
+    });
 }
 
 function drawCanvas(size) {
     const numberOfSquares = size * size;
 
-    for (let i=0; i < numberOfSquares; i++) {
-        /* TODO: add logic to create square */    
+    for (let i = 0; i < numberOfSquares; i++) {
+        const square = document.createElement('div');
+        square.classList = 'grid-square';
+        square.style = `width: ${Math.floor(CANVASSIZE / size)}px; height: ${Math.floor(CANVASSIZE / size)}px;`;
+
+        canvas.appendChild(square);
     }
+}
+
+function colorSquare(square) {
+    // square.classList = '';
+    // square.classList = `grid-square ${getSquareColor()}`;
+    square.style.backgroundColor = `${getSquareColor()}`;
+}
+
+function getSquareColor() {
+    return 'black';
 }
